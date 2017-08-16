313b5ccd93f1af618a468b33387593864d26cd2e
Jeff <iknowcodesoup@gmail.com>
2017-08-16 15:25:00 -0400

    Key import update

--- src/server/authSetup.ts
+++ src/server/authSetup.ts
@@ -1,18 +1,19 @@
 import {
     Server
 } from 'hapi';
-import { appKey, secretKey } from './config';
 import validateRequest from './validateRequest';
 import { Buffer } from 'buffer';
+import privateKey from './keys/private_key';
+import svappKey from './keys/svapp_key';
 
 const authSetup = (server: Server) => {
     server.auth.strategy('jwt', 'jwt', 'required',
         {
-            key: new Buffer(secretKey, 'base64'),
+            key: new Buffer(privateKey, 'base64'),
             validateFunc: validateRequest,
             verifyOptions: {
                 algorithms: ['HS256'],
-                audience: appKey
+                audience: svappKey
             }
         });
 };
--- src/server/config.ts
+++ src/server/config.ts
@@ -10,7 +10,5 @@ const hapiConfig: IServerConnectionOptions = {
 };
 
 const dbUrl: string = 'mongodb://localhost:27017/hapi-app';
-const secretKey: string = 'khLCm6gl4iMG6h4VstmyZ8T3GSbgEx9kbIxcmQmQOBfOFZ+NM0Tm4dYVhCFkwg4DeUljGFmI5mVLwk6DhNdkUvlMVFQ9ZLqO1jERb/H3+coJjBXsayKEkhdIh4e7c1QUmWuk8AxkxRKCXgNXHdsFCc13CZxq4oEo05i5EKAG0RiF/Kl2qNOcJcbCscBRAzICMeplnyeJ0RffLr8k33+pjpa6cMBY4kN8IOynPMkgKCPzuaOYyrlXXvbmpzK2UA/8xh+2EncsKZYeEE9baQYcpzYMBGsoRz3iXZPHNzXrjSjUieARm+8jqiFMww6nM15RjMTnzfSmBpPQDT5TML1o8w==';
-const appKey: string = "";
 
-export { appKey, hapiConfig, dbUrl, secretKey };
+export { hapiConfig, dbUrl };
